4988dbd1f4770c6fa4063749f220507f62472c0f
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-03-12 16:24:15 -0600

    updating image locations and css

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-index
+alert('Welcome to Daniel\'s portfolio: this page is still under construction');
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 4000,
+  wrap: false
+})
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+ })
