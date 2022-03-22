87ac418067946742b4664b24fa42b0c8101f94df
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-03-22 17:48:28 -0500

    updated map javascript

---
+++ map.js
@@ -0,0 +1,12 @@
+var token = config.MapboxAPIToken;
+const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [-66.4069444,18.3277778],
+    zoom: 10
+});
+var marker = new mapboxgl.Marker()
+    .setLngLat([-66.4069444,18.3277778])
+    .addTo(map);
+
+    var worldMarkers = []
