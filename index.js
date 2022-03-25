343a9b6bdad372e3c8be31778ffc6db5e04e0e40
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-03-25 13:50:24 -0500

    working on portfolio

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('this page is still under construction');
+var images = new Array();
+
+function preloadImages() {
+
+    for (i = 0; i < preloadImages.arguments.length; i++) {
+
+        images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
+}
+
+preloadImages("TheTwoOfUs.jpg", "html-css-js-logo.png", "ReactTransparentLogo.png", "MERNTransparentLogo.png");
