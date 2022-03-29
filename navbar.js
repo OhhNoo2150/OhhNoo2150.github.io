ab33849be0440551b3d50dea188f7e56dac6a569
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-03-29 18:55:19 -0500

    add JS to navbar, working on adding JS to footer updated overall look

---
+++ navbar.js
@@ -0,0 +1,27 @@
+const header = document.getElementById('header');
+const myNavBar = `
+<nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
+<div class="container-fluid" id="nav-wrap">
+  <a id="navbar-brand" class="navbar-brand"><img class="logo" src="./Images/globeLogoWaterThree.gif">Daniel Rivera's Portfolio</a>
+  <ul id="navbar-nav" class="navbar-nav d-flex flex-row bd-highlight">
+    <li class="nav-item">
+      <a
+        class="nav-link active"
+        aria-current="page"
+        href="./index.html"><img class="homeButton" src="./Images/PR_Home_Button.png" alt="home button"></a>
+     </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="./about.html"><img class="aboutButton" src="./Images/PR_about_button.png" alt="about button"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="./projects.html"><img class="projectsButton" src="./Images/PR_projects_button.png" alt="projects button"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" href="./map.html"><img class="mapButton" src="./Images/PR_Map_button.png" alt="map button"></a>
+    </li>
+  </ul>
+</div>
+</nav>
+`;
+
+const createNavBar = () => header.innerHTML = myNavBar;
