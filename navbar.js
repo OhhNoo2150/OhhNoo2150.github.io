164d0840a2b8a4bf91486754863a8800a37aaed7
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-03-31 18:27:33 -0500

    updating navbar, and styling to text and page

--- navbar.js
+++ navbar.js
@@ -2,22 +2,22 @@ const header = document.getElementById('header');
 const myNavBar = `
 <nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
 <div class="container-fluid" id="nav-wrap">
-  <a id="navbar-brand" class="navbar-brand">Daniel Rivera</a>
+  <a id="navbar-brand" class="navbar-brand"><img src="./Images/TitleImageForNavbar.png" alt="Daniel Rivera over Taino Coqui"></a>
   <ul id="navbar-nav" class="navbar-nav d-flex flex-row bd-highlight">
     <li class="nav-item">
       <a
         class="nav-link active"
         aria-current="page"
-        href="./index.html"><img class="homeButton" src="./Images/RedoRicanFlagHome.jpeg" alt="home button"></a>
+        href="./index.html"><img class="homeButton" src="./Images/NavbarHomeButton.gif" alt="home button"></a>
      </li>
     <li class="nav-item">
-      <a class="nav-link active" href="./about.html"><img class="aboutButton" src="./Images/RedoRicanFlagAbout.jpeg" alt="about button"></a>
+      <a class="nav-link active" href="./about.html"><img class="aboutButton" src="./Images/NavbarAboutButton.gif" alt="about button"></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link active" href="./projects.html"><img class="projectsButton" src="./Images/RedoRicanFlagProjects.jpeg" alt="projects button"></a>
+      <a class="nav-link active" href="./projects.html"><img class="projectsButton" src="./Images/NavbarProjectsButton.gif" alt="projects button"></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link active" href="./map.html"><img class="mapButton" src="./Images/RedoRicanFlagMap.jpeg" alt="map button"></a>
+      <a class="nav-link active" href="./map.html"><img class="mapButton" src="./Images/NavbarMapButton.gif" alt="map button"></a>
     </li>
   </ul>
 </div>
