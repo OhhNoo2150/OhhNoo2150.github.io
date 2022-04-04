5299ed2a93428f52b9b92f6ae16e770ec896e212
Daniel Rivera <daniel.i.rivera@protonmail.com>
2022-04-04 12:14:02 -0500

    updated copyright styling and buttons for project page

--- index.js
+++
@@ -1,9 +0,0 @@
-let mouseCursor = document.querySelector(".cursor")
-let lis = document.querySelectorAll("body")
-
-window.addEventListener("mouseover", cursor)
-
-function cursor(e) {
-    mouseCursor.style.top = e.pageY + "px"
-    mouseCursor.style.left = e.pageX + "px"
-}
